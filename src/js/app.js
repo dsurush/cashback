3bed1a63787fe58602ddcdb80123d674331b4c95
Surush <davlatzoda.surush@gmail.com>
2020-01-07 10:14:27 +0500

    Initial version

---
+++ src/js/app.js
@@ -0,0 +1,11 @@
+// ctrl + ` - open terminal
+//npm init -  иниициировать проект . создается package json
+//npm start - запускает live server 
+
+const regularPurchases = 2000;
+// 0.01 - magic value (magic constant, hardcoded)
+const regularPurchasesPercent = 0.01;
+const cashback = regularPurchases * regularPurchasesPercent;
+console.log(cashback);
+
+// gist
