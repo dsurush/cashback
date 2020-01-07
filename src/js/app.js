cd5347e448aa9a74bbe40b1c3db59e5e02967e7a
Surush <davlatzoda.surush@gmail.com>
2020-01-07 10:56:39 +0500

    Cashback limit added

--- src/js/app.js
+++ src/js/app.js
@@ -2,10 +2,14 @@
 //npm init -  иниициировать проект . создается package json
 //npm start - запускает live server 
 
-const regularPurchases = 2000;
+const regularPurchases = 2000000000;
 // 0.01 - magic value (magic constant, hardcoded)
 const regularPurchasesPercent = 0.01;
-const cashback = regularPurchases * regularPurchasesPercent;
-console.log(cashback);
+let cashback = regularPurchases * regularPurchasesPercent;
 
+const cashbackLimit = 3000;
+if(cashback > cashbackLimit){
+    cashback = cashbackLimit;
+}
+console.log(cashback);
 // gist
